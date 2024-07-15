4373adf90b69d9ee3d9d46a0beec72720aaefd18
AMOO <amoosamson@gmail.com>
2024-07-15 09:05:50 +0100

    first commit

---
+++ client/src/components/Filter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <input v-model="filterText" type="text" placeholder="Filter">
+      <ul>
+        <li v-for="item in filteredList" :key="(link unavailable)">{{ item.name }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        dataList: [{ id: 1, name: 'Item 1' }, { id: 2, name: 'Item 2' }, ...],
+        filterText: ''
+      }
+    },
+    computed: {
+      filteredList() {
+        return this.dataList.filter(item => item.name.includes(this.filterText))
+      }
+    }
+  }
+  </script>
